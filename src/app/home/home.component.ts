8dc76758afc4a1db15e373e4776927d598c8b8af
Fazliddin Khayrullaev <fazliddinkhayrullaev4@gmail.com>
2024-03-25 22:20:12 +0500

    lesson-4 food images

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../services/food/food.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  constructor(private foodService:FoodService){}
+  foods:String[]=[]
+  ngOnInit(): void {
+    this.foods=this.foodService.getAll()
+  }
+
+}
