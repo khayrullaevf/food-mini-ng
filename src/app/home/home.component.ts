8c4e24f6aa63a0ac34cbdbb8b7abc94ddfdcc022
khayrullaevf <fazliddinkhayrullaev4@gmail.com>
2024-03-26 08:34:35 +0300

    Lesson-7 search logic

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FoodService } from '../services/food/food.service';
 import { Food } from '../shared/models/Food';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,10 +9,17 @@ import { Food } from '../shared/models/Food';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private foodService:FoodService){}
+  constructor(private foodService:FoodService,private route:ActivatedRoute){}
   foods:Food[]=[]
   ngOnInit(): void {
-    this.foods=this.foodService.getAll()
+    this.route.params.subscribe(params=>{
+      if(params['searchTerm']){
+        this.foods=this.foodService.getAll().filter(food=>food.name.toLowerCase().includes(params['searchTerm'].toLowerCase()))
+      }else{
+        this.foods=this.foodService.getAll()
+      }
+    })
   }
 
 }
+
