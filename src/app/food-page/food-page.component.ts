ebbc9339be6cbfc415e4896eca0321fb8d3192c7
khayrullaevf <fazliddinkhayrullaev4@gmail.com>
2024-03-26 14:05:42 +0300

    Lesson-23 add-to-cart logic

--- src/app/food-page/food-page.component.ts
+++ src/app/food-page/food-page.component.ts
@@ -1,7 +1,8 @@
 import { FoodService } from './../services/food/food.service';
 import { Component, OnInit } from '@angular/core';
 import { Food } from '../shared/models/Food';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CartService } from '../services/cart/cart.service';
 
 @Component({
   selector: 'app-food-page',
@@ -14,7 +15,7 @@ export class FoodPageComponent implements OnInit{
   food!:Food;
 
 
-constructor(private activatedRoute:ActivatedRoute,private foodService:FoodService){
+constructor(private activatedRoute:ActivatedRoute,private foodService:FoodService,private cartService:CartService,private router:Router){
   activatedRoute.params.subscribe(params=>{
     if(params['id']){
       this.food=foodService.getFoodById(params['id'])
@@ -22,8 +23,15 @@ constructor(private activatedRoute:ActivatedRoute,private foodService:FoodServic
   })
 }
 ngOnInit(): void {
-
-
-
 }
+
+
+addToCart(){
+  this.cartService.addToCart(this.food)
+  this.router.navigateByUrl('/cart-page')
+}
+
+
+
+
 }
